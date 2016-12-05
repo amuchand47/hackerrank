bed292fdc4beb6665c6e2209b738bd7342a6021b
MD. CHAND ALAM <csadpeb@gmail.com>
2016-12-05 06:31:15 +0530

    Create Circular_rotation.cpp

---
+++ Circular_rotation.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main() 
+{
+  int n,k,q; // k'th rotation 
+  cin>>n>>k>>q; 
+  int a[n],a1[n]; //create another same array
+
+for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        a1[i]=a[i];      //fill both array with same element 
+    }
+
+for(int i=0;i<q;i++)
+ {
+    int t;
+    cin>>t;
+    for(int i=0;i<n;i++)
+    a1[(i+k)%n]=a[i] ;               // now just copy from 1st to 2nd one array by (i+k)%n 
+    cout<<a1[t]<<endl;                // if u want to rotate left then just swap a1[i]=a[(i+k)%n]
+}
+return 0;
+}
